2695bb40b2ac84a79ba553148574b638bc5b2dd3
StefankinaO <60422917+StefankinaOlya@users.noreply.github.com>
2023-08-31 18:57:28 +0300

    Create realization.py

---
+++ Delete Duplicate Emails/realization.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Modify Person in place
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by='id',ascending=True,inplace=True)
+    # Drop the duplicates based on email.
+    person.drop_duplicates(subset='email', keep='first', inplace=True)
