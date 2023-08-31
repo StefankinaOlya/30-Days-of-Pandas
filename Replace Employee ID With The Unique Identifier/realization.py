4f06b17fdd2a2182ee42f0993d2bc477347ff09a
StefankinaO <60422917+StefankinaOlya@users.noreply.github.com>
2023-08-31 19:24:43 +0300

    Create realization.py

---
+++ Replace Employee ID With The Unique Identifier/realization.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    merged_df = pd.merge(employees, employee_uni, how='left', on='id')
+    result_df = merged_df[['unique_id', 'name']]
+    
+    return result_df
